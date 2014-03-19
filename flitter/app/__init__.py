106fcbf9013e93ad92ece78980983852b8a28936
jbrambleDC <jordanbramble@gmail.com>
2014-03-19 02:38:37 -0400

    Update __init__.py

--- flitter/app/__init__.py
+++ flitter/app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 import os
 from flask.ext.login import LoginManager
-from flask.ext.openid import OpenID
+#from flask.ext.openid import OpenID
 from config import basedir
 from flask.ext.sqlalchemy import SQLAlchemy
 
